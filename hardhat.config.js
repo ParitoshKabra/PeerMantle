a9978a09f66a7042bbd0eca0f21b663838869fa4
Paritosh Kabra <paritosh_k@cs.iitr.ac.in>
2023-02-13 11:12:45 +0530

    Setup HardHat Config for Mantle

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    "mantle-testnet": {
+      url: "https://rpc.testnet.mantle.xyz/",
+      accounts: [process.env.PRIV_KEY] // Uses the private key from the .env file
+    }
+  }
+};
